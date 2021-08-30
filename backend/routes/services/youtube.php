015b211e0a44ebf9b385c83afe1f70eb00ef3a6f
Jacek Korzemski <wrp40.000@wp.pl>
2021-08-30 18:26:05 +0200

    Becouse of security reasons, removed update API routes (now, you can update only via CLI). Also added install.sql to build clean DB from start.

--- backend/routes/services/youtube.php
+++ backend/routes/services/youtube.php
@@ -1,59 +1,16 @@
 <?php 
 
-route('GET', '^/youtube/update_channel/(.*?)$', function() {
-  if (isset($_GET['id']))
-  {
-    $s = new Youtube();
-    header('Content-Type: application/json');
-    if (isset($_GET['page']))
-    {
-      if (isset($_GET['validate']) && $_GET['validate'] == true)
-      {
-        $s->updateChannel($_GET['id'], $_GET['page'], false, true);
-      }
-      else {
-        $s->updateChannel($_GET['id'], $_GET['page']);
-      }
-    }
-    else
-    {
-      if (isset($_GET['validate']) && $_GET['validate'] == true)
-      {
-        $s->updateChannel($_GET['id'], false, false, true);
-      }
-      else
-      {
-        $s->updateChannel($_GET['id']);
-      }
-    }
-  }
-  else
-  {
-    throw new Error('Missing ?id= parameter. Cannot perform channel update.');
-  }
-});
-
-route('GET', '^/youtube/get_channel_by_name/(.*?)$', function() {
+route('POST', '^/youtube/get_channel_by_name/(.*?)$', function() {
   $s = new Browse();
-  $s->getChannelByName($_GET['channel']);
+  $s->getChannelByName($_POST['channel'], $_POST['userId'], $_POST['token']);
 });
 
-route('GET', '^/youtube/get_channel_by_id/(.*?)$', function() {
+route('POST', '^/youtube/get_channel_by_id/(.*?)$', function() {
   $s = new Browse();
-  $s->getChannelById($_GET['channel']);
+  $s->getChannelById($_POST['channel'], $_POST['userId'], $_POST['token']);
 });
 
-route('GET', '^/youtube/get_video/(.*?)$', function() {
+route('POST', '^/youtube/get_video/(.*?)$', function() {
   $s = new Browse();
-  $s->getVideo($_GET['id']);
-});
-
-route('GET', '^/youtube/update_channels_list$', function() {
-  $s = new Youtube();
-  $s->updateChannelsList();
-});
-
-route('GET', '^/youtube/update_all_channels$', function() {
-  $s = new Youtube();
-  $s->updateAllChannels();
+  $s->getVideo($_POST['id'], $_POST['userId'], $_POST['token']);
 });
--- sql/install.sql
+++ sql/install.sql
@@ -1,1 +1,72 @@
-# test
+/* Initial Settings */
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/* Create Users Table */
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `power` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+/* Create tokens table */
+
+CREATE TABLE `tokens` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `expiredDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `tokens`
+  ADD PRIMARY KEY (`id`);
+
+/* Create channels table */
+
+CREATE TABLE `channels` (
+  `id` int(11) NOT NULL,
+  `channel_id` varchar(255) NOT NULL,
+  `channel_title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `channels`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `channel_id` (`channel_id`);
+
+ALTER TABLE `channels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+/* Creater music table */
+
+CREATE TABLE `music` (
+  `id` double NOT NULL,
+  `channel_id` varchar(255) NOT NULL,
+  `video_id` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `published_at` datetime NOT NULL,
+  `default_thumbnail` text NOT NULL,
+  `medium_thumbnail` text NOT NULL,
+  `high_thumbnail` text NOT NULL,
+  `channel_title` varchar(255) NOT NULL,
+  `hide` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `music`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `video_id` (`video_id`);
+
+ALTER TABLE `music`
+  MODIFY `id` double NOT NULL AUTO_INCREMENT;
+
+COMMIT;
