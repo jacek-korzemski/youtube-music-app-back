e09bdeae0e1be409450081decb47c33ccd2105b9
Jacek Korzemski <wrp40.000@wp.pl>
2021-09-16 01:17:29 +0200

    Backend - new methods and routes for managing playlists

--- backend/routes/services/user.php
+++ backend/routes/services/user.php
@@ -9,11 +9,35 @@ route('POST', '^/getUserData$', function() {
 route('POST', '^/subscribeChannel$', function() {
   $s = new User();
   header('Content-Type: application/json');
-  echo $s->getUserData($_POST['userId'], $_POST['token'], $_POST['channelId']);
+  echo $s->subscribeChannel($_POST['userId'], $_POST['token'], $_POST['channelId']);
 });
 
 route('POST', '^/unsubscribeChannel$', function() {
   $s = new User();
   header('Content-Type: application/json');
-  echo $s->getUserData($_POST['userId'], $_POST['token'], $_POST['channelId']);
+  echo $s->unsubscribeChannel($_POST['userId'], $_POST['token'], $_POST['channelId']);
+});
+
+route('POST', '^/createPlaylists$', function() {
+  $s = new User();
+  header('Content-Type: application/json');
+  echo $s->createPlaylists($_POST['userId'], $_POST['token'], $_POST['playlistName'], $_POST['playlistDescription']);
+});
+
+route('POST', '^/removePlaylist$', function() {
+  $s = new User();
+  header('Content-Type: application/json');
+  echo $s->removePlaylist($_POST['userId'], $_POST['token'], $_POST['playlistId']);
+});
+
+route('POST', '^/addRecordToPlaylist$', function() {
+  $s = new User();
+  header('Content-Type: application/json');
+  echo $s->getUserData($_POST['userId'], $_POST['token'], $_POST['playlistId'], $_POST['recordId']);
+});
+
+route('POST', '^/removeRecordFromPlaylist$', function() {
+  $s = new User();
+  header('Content-Type: application/json');
+  echo $s->getUserData($_POST['userId'], $_POST['token'], $_POST['playlistRecordId']);
 });
--- backend/services/user/User.php
+++ backend/services/user/User.php
@@ -20,44 +20,64 @@ class User
     return '{"code": 200, "status": "success", "message": "Successfully fetched user data.", "data": '.json_encode($data).'}';
   }
 
-  public function subscribeChannel($userId, $clientToken, $channel_id)
+  public function subscribeChannel($userId, $clientToken, $channelId)
   {
     if (!$this->validation($userId, $clientToken))
     {
       return '{"code": 401, "status": "error", "message": "Cannot authenticate user."}';
     }
-    $this->db->query("INSERT INTO `subscriptions` (`id`, `user_id`, `channel_id`) VALUES (NULL, '$userId', '$channel_id');");
+    $this->db->query("INSERT INTO `subscriptions` (`id`, `user_id`, `channel_id`) VALUES (NULL, '$userId', '$channelId');");
     return '{"code": 200, "status": "success", "message": "Successfully subscribed channel."}';
   }
 
   public function unsubscribeChannel()
   {
-    if (!$this->validation($userId, $clientToken, $channel_id))
+    if (!$this->validation($userId, $clientToken))
     {
       return '{"code": 401, "status": "error", "message": "Cannot authenticate user."}';
     }
-    $this->db->query("DELETE FROM `subscriptions` WHERE `userId` = '$userId' AND `channel_id` = '$channel_id'");
+    $this->db->query("DELETE FROM `subscriptions` WHERE `userId` = '$userId' AND `channel_id` = '$channelId'");
     return '{"code": 200, "status": "success", "message": "Successfully unsubscribed channel."}';
   }
 
-  public function createPlaylists()
+  public function createPlaylists($userId, $clientToken, $playlistName, $playlistDescription)
   {
-
+    if (!$this->validation($userId, $clientToken))
+    {
+      return '{"code": 401, "status": "error", "message": "Cannot authenticate user."}';
+    }
+    $this->db->query("INSERT INTO `playlists` (`id`, `user_id`, `playlist_name`, `playlist_description`) VALUES (NULL, '$userId', '$playlistName', '$playlistDescription')");
+    return '{"code": 200, "status": "success", "message": "Successfully created playlist."}';
   }
 
   public function removePlaylist()
   {
-
+    if (!$this->validation($userId, $clientToken, $playlistId))
+    {
+      return '{"code": 401, "status": "error", "message": "Cannot authenticate user."}';
+    }
+    $this->db->query("DELETE FROM `playlists` WHERE `id` = '$playlistId'");
+    return '{"code": 200, "status": "success", "message": "Successfully removed playlist."}';
   }
 
-  public function addRecordToPlaylist()
+  public function addRecordToPlaylist($userId, $clientToken, $playlistId, $recordId)
   {
-
+    if (!$this->validation($userId, $clientToken, $playlistId))
+    {
+      return '{"code": 401, "status": "error", "message": "Cannot authenticate user."}';
+    }
+    $this->db->query("INSERT INTO `playlists_records` (`id`, `playlist_id`, `record_id`) VALUES (NULL '$playlistId', '$recordId')");
+    return '{"code": 200, "status": "success", "message": "Successfully added record to playlist."}';
   }
 
-  public function removeRecordFromPlaylist()
+  public function removeRecordFromPlaylist($userId, $clientToken, $playlistRecordId)
   {
-    
+    if (!$this->validation($userId, $clientToken))
+    {
+      return '{"code": 401, "status": "error", "message": "Cannot authenticate user."}';
+    }
+    $this->db->query("DELETE FROM `playlists_records` WHERE `id` = '$playlistRecordId'");
+    return '{"code": 200, "status": "success", "message": "Successfully removed record from playlist."}';
   }
 
   public function voteForRecord()
@@ -80,7 +100,7 @@ class User
 
   }
 
-  private function validate()
+  private function validation($userId, $clientToken)
   {
     $validation = json_decode($this->auth->checkToken($userId, $clientToken));
     if ($validation && $validation->code == 200)
